16c8b4eef0555fe486b9a93c84a69fb2bc6b9c49
Mark Haskamp <mark.haskamp@pinnsg.com>
2010-02-18 14:29:56 -0500

    back to sanity

--- javascript/dateValidation/js/dateValidate.js
+++ javascript/dateValidation/js/dateValidate.js
@@ -1,20 +1,4 @@
 
-var userMonthNdx = { javascript: 0
-                     ,user: 1
-                     ,descr: 2 };
-
-var userMonth = { 1: [ 0, 1, 'january' ]
-                  ,2: [ 1, 2, 'february' ]
-                  ,3: [ 2, 3, 'march' ]
-                  ,4: [ 3, 4, 'april' ]
-                  ,5: [ 4, 5, 'may' ]
-                  ,6: [ 5, 6, 'june' ]
-                  ,7: [ 6, 7, 'july' ]
-                  ,8: [ 7, 8, 'august' ]
-                  ,9: [ 8, 9, 'september' ]
-                  ,10: [ 9, 10, 'october' ]
-                  ,11: [ 10, 11, 'november' ]
-                  ,12:  [ 11, 12, 'december' ] };
     
 function isValidDate(dateStr) {
   // copied from http://www.codetoad.com/javascript_IsValidDate.asp
@@ -26,13 +10,13 @@ function isValidDate(dateStr) {
   }
 
   var month = matchArray[1]; 
-  var jsmonth = userMonth[month][userMonthNdx.javascript];
+  month--;
   var day   = matchArray[2]; 
   var year  = matchArray[3]; 
 
-var javascriptDate = new Date(year, jsmonth, day);
+var javascriptDate = new Date(year, month, day);
   return((day == javascriptDate.getDate()) &&
-         (jsmonth == javascriptDate.getMonth()) &&
+         (month == javascriptDate.getMonth()) &&
          (year == javascriptDate.getFullYear()));
 
 }
