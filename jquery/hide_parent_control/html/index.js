d861ce045e51da7d33ab12f3e55529727f1965f3
Mark Haskamp <mrhask@gmail.com>
2010-06-15 00:01:27 -0400

    hide_parent_control method in index.js

--- jquery/hide_parent_control/html/index.js
+++ jquery/hide_parent_control/html/index.js
@@ -1,3 +1,22 @@
+function hide_parent_control(control_array, parent_control) {
+
+    var counter = 0;
+    $.each(control_array, function(ndx, value) {
+
+	if (control_exists(value)) {
+	    counter = counter + 1;
+	}
+    });
+
+    if (counter === 0) {
+	$(parent_control).hide();
+    }
+}
+
+function control_exists(ctl_selector) {
+    return ($(ctl_selector).length);
+}
+
 
 $(document).ready(function() {
 
